95ca5f972e0d369aeb01e5a1427c60f0ffb54ba6
Sergey Aksyonov <sergey@aksyonov.pp.ua>
2013-11-17 01:26:06 +0200

    added main files

---
+++ angular-storageSync.js
@@ -0,0 +1,32 @@
+angular.module('storageSync', [])
+  .factory('$storageSync', function($rootScope) {
+    'use strict';
+
+    function StorageSync(name, valFn, def) {
+      if (def === undefined) {
+        def = {};
+      }
+      listeners[name] = $rootScope.$watch(valFn, function(value) {
+        if (valFn !== undefined) {
+          localStorage[name] = JSON.stringify(value);
+        } else {
+          delete localStorage[name];
+        }
+      }, true);
+
+      return localStorage[name] !== undefined ?
+        JSON.parse(localStorage[name]) : def;
+    }
+
+    var listeners = {};
+
+    StorageSync.stop = function(name) {
+      listeners[name]();
+    };
+
+    StorageSync.remove = function(name) {
+      delete localStorage[name];
+    };
+
+    return StorageSync;
+  });
